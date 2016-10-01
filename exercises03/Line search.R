44829eeb5f33d3d152785703cbff3108cacc04de
MatteoSDS <matteo.vestrucci@utexas.edu>
2016-10-01 12:45:38 -0500

    files ex03 line search

---
+++ exercises03/Line search.R
@@ -0,0 +1,109 @@
+library(Matrix)
+library(microbenchmark)
+
+negloglikelihood<-function(m,y,X,beta){
+  total<-0
+  N<-length(y)
+  for(i in 1:N) total<-total+(m[i]-y[i])*t(X[i,])%*%beta+m[i]*log(1+exp(-t(X[i,])%*%beta))
+  return(total)
+}
+
+gradient_negloglik<-function(m,y,X,beta){
+  w<-1/(1+exp(-X%*%beta))
+  S<-m*w-y
+  grad<-t(X)%*%S
+  return(grad)
+}
+
+gradient_descent<-function(m,y,X,beta0,stepsize,maxstepnumber,
+                           accuracy_obj_fun,accuracy_beta_val){
+  negloglik<-numeric(maxstepnumber)
+  negloglik[1]<-negloglikelihood(m,y,X,beta0)
+  gradient<-gradient_negloglik(m,y,X,beta0)
+  diff_beta_val<-accuracy_beta_val+1
+  diff_obj_fun<-accuracy_obj_fun+1
+  i<-1
+  while(!(i==maxstepnumber)&&
+        ((accuracy_beta_val<diff_beta_val)||
+         (accuracy_obj_fun<diff_obj_fun))){
+    i<-i+1
+    beta1<-beta0-stepsize*gradient
+    diff_beta_val<-sum(abs(beta0-beta1))
+    beta0<-beta1
+    negloglik[i]<-negloglikelihood(m,y,X,beta0)
+    diff_obj_fun<-negloglik[i-1]-negloglik[i]
+    gradient<-gradient_negloglik(m,y,X,beta0)
+  }
+  return(list(betahat=beta0,negloglik=negloglik,step=i))
+}
+
+line_search<-function(alpha,c1,rho,gradient,m,y,X,beta){
+  while(negloglikelihood(m,y,X,(beta-alpha*gradient))>
+        negloglikelihood(m,y,X,beta)+c1*alpha*crossprod(gradient))
+        alpha<-alpha*rho
+  return(alpha)
+}
+
+line_search_gd<-function(m,y,X,beta0,maxstepnumber,alpha,c1,rho,
+                           accuracy_obj_fun,accuracy_beta_val){
+  negloglik<-numeric(maxstepnumber)
+  negloglik[1]<-negloglikelihood(m,y,X,beta0)
+  gradient<-gradient_negloglik(m,y,X,beta0)
+  diff_beta_val<-accuracy_beta_val+1
+  diff_obj_fun<-accuracy_obj_fun+1
+  i<-1
+  while(!(i==maxstepnumber)&&
+        ((accuracy_beta_val<diff_beta_val)||
+         (accuracy_obj_fun<diff_obj_fun))){
+    i<-i+1
+    stepsize<-line_search(alpha,c1,rho,gradient,m,y,X,beta0)
+    beta1<-beta0-stepsize*gradient
+    diff_beta_val<-sum(abs(beta0-beta1))
+    beta0<-beta1
+    negloglik[i]<-negloglikelihood(m,y,X,beta0)
+    diff_obj_fun<-negloglik[i-1]-negloglik[i]
+    gradient<-gradient_negloglik(m,y,X,beta0)
+  }
+  return(list(betahat=beta0,negloglik=negloglik,step=i))
+}
+
+data_wdbc<-read.csv("./wdbc.csv", header=FALSE)
+X<-as.matrix(cbind(rep(1,569),scale(data_wdbc[,3:12])))
+y<-data_wdbc[,2]
+y<-as.numeric(y=="M")
+m<-rep(1,569)
+
+beta0<-rep(0,11)
+stepsize<-0.02
+alpha<-0.06
+c1<-0.0001
+rho<-2/3
+maxstepnumber<-1000
+accuracy_obj_fun<-0.001
+accuracy_beta_val<-0.001
+
+result_grad_desc<-gradient_descent(m,y,X,beta0,stepsize,maxstepnumber,
+                                   accuracy_obj_fun,accuracy_beta_val)
+result_line_search<-line_search_gd(m,y,X,beta0,maxstepnumber,
+                                   alpha,c1,rho,
+                                   accuracy_obj_fun,accuracy_beta_val)
+
+result_grad_desc$betahat
+result_line_search$betahat
+
+plot(result_grad_desc$negloglik[1:result_grad_desc$step],
+     main = "Negative Log-likelihood at each step",
+     xlab="Step",ylab="negloglik",type="l",log="xy")
+points(result_line_search$negloglik[1:result_line_search$step],
+       type="l",lty=2)
+text(1, 300, "GD")
+text(3, 300, "GD_LS")
+
+maxstepnumber<-100
+comparison<-microbenchmark(times=10,
+result_grad_desc<-gradient_descent(m,y,X,beta0,stepsize,maxstepnumber,
+                                   accuracy_obj_fun,accuracy_beta_val),
+result_line_search<-line_search_gd(m,y,X,beta0,maxstepnumber,
+                                   alpha,c1,rho,
+                                   accuracy_obj_fun,accuracy_beta_val))
+comparison
